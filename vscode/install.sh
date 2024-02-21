5ccecce2a25a03bd091d39193f6e6bdcb8d02370
Kirill Sumorokov <sumorokov.k@gmail.com>
2024-02-21 11:37:50 +0500

    VSCode settings added

---
+++ vscode/install.sh
@@ -0,0 +1,31 @@
+# Common
+code --install-extension editorconfig.editorconfig
+code --install-extension tamasfe.even-better-toml
+
+# Themes
+code --install-extension jolaleye.horizon-theme-vscode
+code --install-extension emmanuelbeziat.vscode-great-icons
+
+# Tools
+code --install-extension alefragnani.Bookmarks
+code --install-extension eamodio.gitlens
+code --install-extension github.vscode-github-actions
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension humao.rest-client
+
+# Ansible development
+code --install-extension redhat.ansible
+code --install-extension redhat.vscode-yaml
+
+# Python development
+code --install-extension ms-python.python
+code --install-extension ms-python.mypy-type-checker
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.vscode-pylance
+code --install-extension Cameron.vscode-pytest
+code --install-extension charliermarsh.ruff
+
+# Remote development
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode.remote-explorer
